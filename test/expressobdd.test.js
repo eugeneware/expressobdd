844069d1aa6945b78823c6ddba19b3f25bdcbc97
eugene <eugene@noblesamurai.com>
2011-02-12 01:39:16 +1100

    Initial Commit

---
+++ test/expressobdd.test.js
@@ -0,0 +1,26 @@
+require('should');
+
+module.exports = require('../lib/expressobdd')({
+	'group one': {
+		'it should do x': function() {
+			var x = 2;
+			x.should.eql(2);
+		},
+
+		'it should do y': function() {
+			var y = 3;
+			y.should.eql(3);
+		},
+
+		'group one point 2': {
+			'it should do w': function() {
+				var z = 5;
+				z.should.eql(6);
+			},
+		},
+	},
+	'it should do z': function() {
+		var z = 4;
+		z.should.eql(4);
+	},
+});
